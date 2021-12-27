e7917aa1c93a30f46b1efd9ceada3f355df0bca3
John <971890649@qq.com>
2021-12-27 10:04:17 +0800

    静态匹配

--- examples/pointcloud/rs-pcl.cpp
+++ examples/pointcloud/rs-pcl.cpp
@@ -1,25 +1,69 @@
- // License: Apache 2.0. See LICENSE file in root directory.
- // Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
 
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include "../example.hpp" // Include short list of convenience functions for rendering
 
-#include <pcl/point_types.h>
+#include <iostream>
+
 #include <pcl/filters/passthrough.h>
 
- // Struct for managing rotation of pointcloud view
+#include <boost/make_shared.hpp>               //boost指针相关头文件
+#include <pcl/point_types.h>                   //点类型定义头文件
+#include <pcl/point_cloud.h>                   //点云类定义头文件
+#include <pcl/point_representation.h>          //点表示相关的头文件
+#include <pcl/io/pcd_io.h>                     //PCD文件打开存储类头文件
+#include <pcl/filters/voxel_grid.h>             //用于体素网格化的滤波类头文件
+#include <pcl/filters/filter.h>                //滤波相关头文件
+#include <pcl/features/normal_3d.h>            //法线特征头文件
+#include <pcl/registration/icp.h>              //ICP类相关头文件
+#include <pcl/registration/icp_nl.h>           //非线性ICP 相关头文件
+#include <pcl/registration/transforms.h>       //变换矩阵类头文件
+#include <pcl/visualization/pcl_visualizer.h>  //可视化类头文件
+
+#include <unistd.h>
+
+using pcl::visualization::PointCloudColorHandlerCustom;
+using pcl::visualization::PointCloudColorHandlerGenericField;
+
+//定义
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloud; //申明pcl::PointXYZ数据
+typedef pcl::PointNormal PointNormalT;
+typedef pcl::PointCloud<PointNormalT> PointCloudWithNormals;
+
+class MyPointRepresentation : public pcl::PointRepresentation<PointNormalT>
+{
+    using pcl::PointRepresentation<PointNormalT>::nr_dimensions_;
+
+public:
+    MyPointRepresentation()
+    {
+        nr_dimensions_ = 4; //定义点的维度
+    }
+
+    // 重载copyToFloatArray方法将点转化为四维数组
+    virtual void copyToFloatArray(const PointNormalT &p, float *out) const
+    {
+        // < x, y, z, curvature >
+        out[0] = p.x;
+        out[1] = p.y;
+        out[2] = p.z;
+        out[3] = p.curvature; // 曲率
+    }
+};
+
+
+
+// Struct for managing rotation of pointcloud view
 struct state {
     state() : yaw(0.0), pitch(0.0), last_x(0.0), last_y(0.0),
-        ml(false), offset_x(0.0f), offset_y(0.0f) {}
+              ml(false), offset_x(0.0f), offset_y(0.0f) {}
     double yaw, pitch, last_x, last_y; bool ml; float offset_x, offset_y;
 };
 
 using pcl_ptr = pcl::PointCloud<pcl::PointXYZ>::Ptr;
 
- // Helper functions
- //注册状态变量和回调以允许鼠标控制点云
+// Helper functions
 void register_glfw_callbacks(window& app, state& app_state);
- //处理显示点云所需的所有 OpenGL 调用
 void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& points);
 
 pcl_ptr points_to_pcl(const rs2::points& points)
@@ -45,52 +89,96 @@ pcl_ptr points_to_pcl(const rs2::points& points)
 
 float3 colors[] { { 0.8f, 0.1f, 0.3f },
                   { 0.1f, 0.9f, 0.5f },
-                };
+};
 
 int main(int argc, char * argv[]) try
 {
-    // Create a simple OpenGL window for rendering:
+//    // Create a simple OpenGL window for rendering:
     window app(1280, 720, "RealSense PCL Pointcloud Example");
-    // Construct an object to manage view state
+//    // Construct an object to manage view state
     state app_state;
-    // register callbacks to allow manipulation of the pointcloud
+//    // register callbacks to allow manipulation of the pointcloud
     register_glfw_callbacks(app, app_state);
 
-    // Declare pointcloud object, for calculating pointclouds and texture mappings
-    rs2::pointcloud pc;
-    // We want the points object to be persistent so we can display the last cloud when a frame drops
-    rs2::points points;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>);
+    //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
 
-    // Declare RealSense pipeline, encapsulating the actual device and sensors
-    rs2::pipeline pipe;
-    // Start streaming with default recommended configuration
-    pipe.start();
+    pcl::PCDReader reader;
+    reader.read (argv[1], *cloud_in);
 
-    // Wait for the next set of frames from the camera
-    auto frames = pipe.wait_for_frames();
-
-    auto depth = frames.get_depth_frame();
-
-    // Generate the pointcloud and texture mappings
-    points = pc.calculate(depth);
-
-    auto pcl_points = points_to_pcl(points);
-
-    //pcl_ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud(pcl_points);
-    pass.setFilterFieldName("z");
-    pass.setFilterLimits(0.0, 0.25);
-    pass.filter(*pcl_points);
 
     std::vector<pcl_ptr> layers;
-    layers.push_back(pcl_points);
-    //layers.push_back(cloud_filtered);
+    layers.push_back(cloud_in);
+    draw_pointcloud(app, app_state, layers);
 
     while (app) // Application still alive?
+//    {
+//        draw_pointcloud(app, app_state, layers);
+//    }
     {
+
+        rs2::pointcloud pc;
+        rs2::points points;
+
+        // Declare RealSense pipeline, encapsulating the actual device and sensors
+        rs2::pipeline pipe;
+        // Start streaming with default recommended configuration
+        pipe.start();
+
+        // Wait for the next set of frames from the camera
+        auto frames = pipe.wait_for_frames();
+
+        auto depth = frames.get_depth_frame();
+
+        points = pc.calculate(depth);
+        // Generate the pointcloud and texture mappings
+
+        auto clouds = points_to_pcl(points);
+
+
+        pcl::PassThrough<pcl::PointXYZ> pass;
+        pass.setInputCloud(clouds);
+
+        //设置点云距离相机的位置
+        pass.setFilterFieldName("z");
+        pass.setFilterLimits(0.1, 0.25);
+
+
+        pass.filter(*clouds);
+
+        pcl::io::savePCDFileASCII("out.pcd", *clouds);
+        std::cerr << "Saved " << clouds->points.size() << " data points to out.pcd." << std::endl;
+        //cloud_out = clouds;
+        std::vector<pcl_ptr> layers;
+        //        layers.push_back(clouds);
+        layers.push_back(clouds);
         draw_pointcloud(app, app_state, layers);
+
+
+        //        pcl::PCDWriter writer;
+        //        writer.write("./test_pcd.pcd", cloud_filtered, false);
+
+        // 创建IterativeClosestPoint的实例
+        // setInputSource将cloud_in作为输入点云
+        // setInputTarget将平移后的cloud_out作为目标点云
+        pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+        icp.setInputSource(cloud_in);
+        icp.setInputTarget(clouds);
+
+        // 创建一个 pcl::PointCloud<pcl::PointXYZ>实例 Final 对象,存储配准变换后的源点云,
+        // 应用 ICP 算法后, IterativeClosestPoint 能够保存结果点云集,如果这两个点云匹配正确的话
+        // （即仅对其中一个应用某种刚体变换，就可以得到两个在同一坐标系下相同的点云）,那么 icp. hasConverged()= 1 (true),
+        // 然后会输出最终变换矩阵的匹配分数和变换矩阵等信息。
+        pcl::PointCloud<pcl::PointXYZ> Final;
+        icp.align(Final);
+        std::cout << "has converged:" << icp.hasConverged() << " score: " << icp.getFitnessScore() << std::endl;
+        const pcl::Registration<pcl::PointXYZ, pcl::PointXYZ, float>::Matrix4 &matrix = icp.getFinalTransformation();
+        std::cout << matrix << std::endl;
+
     }
+
+
+
 
     return EXIT_SUCCESS;
 }
@@ -105,7 +193,7 @@ catch (const std::exception & e)
     return EXIT_FAILURE;
 }
 
- // Registers the state variable and callbacks to allow mouse control of the pointcloud
+// Registers the state variable and callbacks to allow mouse control of the pointcloud
 void register_glfw_callbacks(window& app, state& app_state)
 {
     app.on_left_mouse = [&](bool pressed)
@@ -143,7 +231,7 @@ void register_glfw_callbacks(window& app, state& app_state)
     };
 }
 
- // Handles all the OpenGL calls needed to display the point cloud
+// Handles all the OpenGL calls needed to display the point cloud
 void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& points)
 {
     // OpenGL commands that prep screen for the pointcloud
@@ -154,13 +242,6 @@ void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& 
 
     glClearColor(153.f / 255, 153.f / 255, 153.f / 255, 1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
- //    glMatrixMode(GLenum mode)
- //    作用：用于指定用哪个矩阵作为当前矩阵，mode用于指定哪一种矩阵栈是其后矩阵操作的目标。mode可取：
- //    GL_MODELVIEW: 把其后的矩阵操作施加于造型视图矩阵栈。（默认）
- //    GL_PROJECTION: 把其后的矩阵操作施加于投影矩阵栈。
- //    GL_TEXTURE： 把其后的矩阵操作施加于纹理矩阵栈。
-
 
     glMatrixMode(GL_PROJECTION);
     glPushMatrix();
@@ -192,6 +273,7 @@ void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& 
         {
             auto&& p = pc->points[i];
             if (p.z)
+
             {
                 // upload the point and texture coordinates only for points we have depth data for
                 glVertex3f(p.x, p.y, p.z);
@@ -201,19 +283,6 @@ void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& 
         glEnd();
     }
 
-    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
-    glBegin(GL_POLYGON);// 设置各个顶点，glBegin开始,glEnd结束
-    glVertex3f(0.0,0.0,0.0);
-    glVertex3f(0.0,0.3,0.0);
-    glVertex3f(0.3,0.0,0.0);
-    glVertex3f(0.3,0.3,0.0);
- //    glVertex3f(0.3,0.0,0.3);
- //    glVertex3f(0.0,0.0,0.3);
- //    glVertex3f(0.0,0.3,0.3);
- //    glVertex3f(0.3,0.3,0.3);
-    glEnd();
-
-
     // OpenGL cleanup
     glPopMatrix();
     glMatrixMode(GL_PROJECTION);
@@ -221,3 +290,5 @@ void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& 
     glPopAttrib();
     glPushMatrix();
 }
+
+
---
+++ examples/pointcloud/static_icp.cpp
@@ -0,0 +1,353 @@
+//
+// Created by john on 2021/12/24.
+//
+
+
+
+/*
+ * @Description:使用迭代最近点算法(ICP)
+ * http://robot.czxy.com/docs/pcl/chapter03/registration/
+ * https://www.cnblogs.com/li-yao7758258/p/6489585.html
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2020-10-22 16:50:22
+ * @LastEditTime: 2020-10-22 17:05:21
+ * @FilePath: /pcl-learning/14registration配准/1使用迭代最近点算法(ICP)/iterative_closest_point.cpp
+ */
+
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+#include "../example.hpp" // Include short list of convenience functions for rendering
+
+#include <iostream>               //标准输入输出头文件
+#include <pcl/io/pcd_io.h>        //I/O操作头文件
+#include <pcl/point_types.h>      //点类型定义头文件
+#include <pcl/registration/icp.h> //ICP配准类相关头文件
+
+#include <ctime>
+// Eigen 部分
+#include <Eigen/Core>
+// 稠密矩阵的代数运算（逆，特征值等）
+#include <Eigen/Dense>
+#include <vector>
+
+using namespace std;
+using pcl_ptr = pcl::PointCloud<pcl::PointXYZ>::Ptr;
+
+struct state {
+    state() : yaw(0.0), pitch(0.0), last_x(0.0), last_y(0.0),
+              ml(false), offset_x(0.0f), offset_y(0.0f) {}
+    double yaw, pitch, last_x, last_y; bool ml; float offset_x, offset_y;
+};
+
+float3 colors[] { { 0.8f, 0.1f, 0.3f },
+                  { 0.1f, 0.9f, 0.5f },
+};
+
+void register_glfw_callbacks(window& app, state& app_state);
+
+void draw_cuboid(window &app, state &app_state, const std::vector<pcl_ptr> &points, const Eigen::Matrix<float, 4, 4> & matrix);
+void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& points);
+
+int main(int argc, char **argv)
+{
+    // Create a simple OpenGL window for rendering:
+    window app(1280, 720, "static icp");
+    // Construct an object to manage view state
+    state app_state;
+    // register callbacks to allow manipulation of the pointcloud
+    //register_glfw_callbacks(app, app_state);
+
+    register_glfw_callbacks(app, app_state);
+
+    // 定义输入和输出点云
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZ>);
+
+    pcl::PCDReader reader;
+    reader.read (argv[1], *cloud_in);
+    reader.read (argv[2], *cloud_out);
+
+
+
+    // //实现一个简单的点云刚体变换，以构造目标点云，将cloud_out中的x平移0.7f米，然后再次输出数据值。
+    for (size_t i = 0; i < cloud_out->points.size(); ++i){
+        cloud_out->points[i].x = cloud_out->points[i].x + 0.7f;
+    }
+
+    pcl::io::savePCDFileASCII("out.pcd", *cloud_out);
+    std::cerr << "Saved " << cloud_out->points.size() << " data points to test_pcd.pcd." << std::endl;
+
+//    // 打印这些点
+//    std::cout << "Transformed " << cloud_in->points.size() << " data points:"
+//              << std::endl;
+//    for (size_t i = 0; i < cloud_out->points.size(); ++i)                         //打印构造出来的目标点云
+//        std::cout << "    " << cloud_out->points[i].x << " " << cloud_out->points[i].y << " " << cloud_out->points[i].z << std::endl;
+
+    // 创建IterativeClosestPoint的实例
+    // setInputSource将cloud_in作为输入点云
+    // setInputTarget将平移后的cloud_out作为目标点云
+    pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
+    icp.setInputSource(cloud_in);
+    icp.setInputTarget(cloud_out);
+
+    // 创建一个 pcl::PointCloud<pcl::PointXYZ>实例 Final 对象,存储配准变换后的源点云,
+    // 应用 ICP 算法后, IterativeClosestPoint 能够保存结果点云集,如果这两个点云匹配正确的话
+    // （即仅对其中一个应用某种刚体变换，就可以得到两个在同一坐标系下相同的点云）,那么 icp. hasConverged()= 1 (true),
+    // 然后会输出最终变换矩阵的匹配分数和变换矩阵等信息。
+    pcl::PointCloud<pcl::PointXYZ> Final;
+    icp.align(Final);
+    std::cout << "has converged:" << icp.hasConverged() << " score: " << icp.getFitnessScore() << std::endl;
+    const pcl::Registration<pcl::PointXYZ, pcl::PointXYZ, float>::Matrix4 &matrix = icp.getFinalTransformation();
+    std::cout << matrix << std::endl;
+
+    std::vector<pcl_ptr> layers;
+    //layers.push_back(pcl_points);
+    layers.push_back(cloud_out);
+    Eigen::Matrix<float, 4, 4> matrix_eig = Eigen::Matrix<float, 4, 4>(matrix);
+
+
+    while (app){
+        //draw_pointcloud(app, app_state, layers);
+        draw_cuboid(app, app_state, layers, matrix_eig);
+    }
+
+    return (0);
+}
+
+// Handles all the OpenGL calls needed to display the point cloud
+void draw_cuboid(window& app, state& app_state, const std::vector<pcl_ptr>& points, const Eigen::Matrix<float, 4, 4> & matrix)
+{
+    // OpenGL commands that prep screen for the pointcloud
+    glPopMatrix();
+    glPushAttrib(GL_ALL_ATTRIB_BITS);
+
+    float width = app.width(), height = app.height();
+
+    glClearColor(153.f / 255, 153.f / 255, 153.f / 255, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    gluPerspective(60, width / height, 0.01f, 10.0f);
+
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    gluLookAt(0, 0, 0, 0, 0, 1, 0, -1, 0);
+
+    glTranslatef(0, 0, +0.5f + app_state.offset_y*0.05f);
+    glRotated(app_state.pitch, 1, 0, 0);
+    glRotated(app_state.yaw, 0, 1, 0);
+    glTranslatef(0, 0, -0.5f);
+
+    glPointSize(width / 640);
+    glEnable(GL_TEXTURE_2D);
+
+    int color = 0;
+
+    for (auto&& pc : points)
+    {
+        auto c = colors[(color++) % (sizeof(colors) / sizeof(float3))];
+
+        glBegin(GL_POINTS);
+        glColor3f(c.x, c.y, c.z);
+
+        /* this segment actually prints the pointcloud */
+        for (int i = 0; i < pc->points.size(); i++)
+        {
+            auto&& p = pc->points[i];
+            if (p.z)
+
+            {
+                // upload the point and texture coordinates only for points we have depth data for
+                glVertex3f(p.x, p.y, p.z);
+            }
+        }
+
+        glEnd();
+    }
+
+    Eigen::Matrix<float, 8, 4> matrix_84;
+    matrix_84 << 0.0, 0.0, 0.2, 1,
+            0.0, 0.1, 0.2, 1,
+            0.1, 0.1, 0.2, 1,
+            0.1, 0.0, 0.2, 1,
+            0.1, 0.0, 0.25, 1,
+            0.0, 0.0, 0.25, 1,
+            0.0, 0.1, 0.25, 1,
+            0.1, 0.1, 0.25, 1;
+    Eigen::Matrix<float, 4, 8> matrix_48 = matrix_84.transpose();
+    matrix_48 = matrix * matrix_48;
+
+//    float vertex_list[][3] =
+//            {
+//                    {0.0, 0.0, 0.2},
+//                    {0.0, 0.1, 0.2},
+//                    {0.1, 0.1, 0.2},
+//                    {0.1, 0.0, 0.2},
+//
+//                    {0.1, 0.0, 0.25},
+//                    {0.0, 0.0, 0.25},
+//                    {0.0, 0.1, 0.25},
+//                    {0.1, 0.1, 0.25}
+//
+//            };
+
+
+
+
+    //绘制一个矩形,默认为填充模式
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+    glBegin(GL_POLYGON);// 设置各个顶点，glBegin开始,glEnd结束
+//    glVertex3f(0.0,0.0,0.2);
+//    glVertex3f(0.0,0.1,0.2);
+//    glVertex3f(0.1,0.1,0.2);
+//    glVertex3f(0.1,0.0,0.2);
+//
+//    glVertex3f(0.1,0.0,0.25);
+//    glVertex3f(0.0,0.0,0.25);
+//    glVertex3f(0.0,0.1,0.25);
+//    glVertex3f(0.1,0.1,0.25);
+
+    glVertex3f(matrix_48(0,0),matrix_48(1,0),matrix_48(2,0));
+    glVertex3f(matrix_48(0,1),matrix_48(1,1),matrix_48(2,1));
+    glVertex3f(matrix_48(0,2),matrix_48(1,2),matrix_48(2,2));
+    glVertex3f(matrix_48(0,3),matrix_48(1,3),matrix_48(2,3));
+    glEnd();
+
+
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+    glBegin(GL_POLYGON);// 设置各个顶点，glBegin开始,glEnd结束
+    glVertex3f(matrix_48(0,3),matrix_48(1,3),matrix_48(2,3));
+    glVertex3f(matrix_48(0,4),matrix_48(1,4),matrix_48(2,4));
+    glVertex3f(matrix_48(0,5),matrix_48(1,5),matrix_48(2,5));
+    glVertex3f(matrix_48(0,0),matrix_48(1,0),matrix_48(2,0));
+    glEnd();
+
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+    glBegin(GL_POLYGON);// 设置各个顶点，glBegin开始,glEnd结束
+    glVertex3f(matrix_48(0,4),matrix_48(1,4),matrix_48(2,4));
+    glVertex3f(matrix_48(0,7),matrix_48(1,7),matrix_48(2,7));
+    glVertex3f(matrix_48(0,6),matrix_48(1,6),matrix_48(2,6));
+    glVertex3f(matrix_48(0,5),matrix_48(1,5),matrix_48(2,5));
+    glEnd();
+
+
+    glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+    glBegin(GL_POLYGON);// 设置各个顶点，glBegin开始,glEnd结束
+    glVertex3f(matrix_48(0,7),matrix_48(1,7),matrix_48(2,7));
+    glVertex3f(matrix_48(0,6),matrix_48(1,6),matrix_48(2,6));
+    glVertex3f(matrix_48(0,1),matrix_48(1,1),matrix_48(2,1));
+    glVertex3f(matrix_48(0,2),matrix_48(1,2),matrix_48(2,2));
+    glEnd();
+
+
+
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glPopAttrib();
+    glPushMatrix();
+}
+
+void register_glfw_callbacks(window& app, state& app_state)
+{
+    app.on_left_mouse = [&](bool pressed)
+    {
+        app_state.ml = pressed;
+    };
+
+    app.on_mouse_scroll = [&](double xoffset, double yoffset)
+    {
+        app_state.offset_x += static_cast<float>(xoffset);
+        app_state.offset_y += static_cast<float>(yoffset);
+    };
+
+    app.on_mouse_move = [&](double x, double y)
+    {
+        if (app_state.ml)
+        {
+            app_state.yaw -= (x - app_state.last_x);
+            app_state.yaw = std::max(app_state.yaw, -120.0);
+            app_state.yaw = std::min(app_state.yaw, +120.0);
+            app_state.pitch += (y - app_state.last_y);
+            app_state.pitch = std::max(app_state.pitch, -80.0);
+            app_state.pitch = std::min(app_state.pitch, +80.0);
+        }
+        app_state.last_x = x;
+        app_state.last_y = y;
+    };
+
+    app.on_key_release = [&](int key)
+    {
+        if (key == 32) // Escape
+        {
+            app_state.yaw = app_state.pitch = 0; app_state.offset_x = app_state.offset_y = 0.0;
+        }
+    };
+}
+
+// Handles all the OpenGL calls needed to display the point cloud
+void draw_pointcloud(window& app, state& app_state, const std::vector<pcl_ptr>& points)
+{
+    // OpenGL commands that prep screen for the pointcloud
+    glPopMatrix();
+    glPushAttrib(GL_ALL_ATTRIB_BITS);
+
+    float width = app.width(), height = app.height();
+
+    glClearColor(153.f / 255, 153.f / 255, 153.f / 255, 1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    gluPerspective(60, width / height, 0.01f, 10.0f);
+
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    gluLookAt(0, 0, 0, 0, 0, 1, 0, -1, 0);
+
+    glTranslatef(0, 0, +0.5f + app_state.offset_y*0.05f);
+    glRotated(app_state.pitch, 1, 0, 0);
+    glRotated(app_state.yaw, 0, 1, 0);
+    glTranslatef(0, 0, -0.5f);
+
+    glPointSize(width / 640);
+    glEnable(GL_TEXTURE_2D);
+
+    int color = 0;
+
+    for (auto&& pc : points)
+    {
+        auto c = colors[(color++) % (sizeof(colors) / sizeof(float3))];
+
+        glBegin(GL_POINTS);
+        glColor3f(c.x, c.y, c.z);
+
+        /* this segment actually prints the pointcloud */
+        for (int i = 0; i < pc->points.size(); i++)
+        {
+            auto&& p = pc->points[i];
+            if (p.z)
+
+            {
+                // upload the point and texture coordinates only for points we have depth data for
+                glVertex3f(p.x, p.y, p.z);
+            }
+        }
+
+        glEnd();
+    }
+
+
+
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glPopAttrib();
+    glPushMatrix();
+}
+
+
+
+
